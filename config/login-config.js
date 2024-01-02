e3ba50c28e0ecffc6528ecf19119f4fd5d90eeae
Galal <gegoah200@gmail.com>
2024-01-03 00:51:52 +0200

    login

---
+++ config/login-config.js
@@ -0,0 +1,37 @@
+const bycrypt =require("bcrypt")
+const Localstrategy =require("passport-local").Strategy 
+
+ function initilaize(passport,getUserByEmail,getUserById){
+    const authenticateuser =async (email,password,done)=>{
+    const user = await getUserByEmail(email)
+    
+    
+    if(user==null){
+return done(null,false,{message:'no user with that email'})
+    }
+    
+   
+     try{if (await bycrypt.compare(password,user.password)){
+        console.log('login user : '+user._id)
+        return done(null,user)
+        
+     }else{
+        return done(null,false,{message :'incorrect password'})
+     }
+     
+    }
+    catch(e){
+    
+        return done(e)
+    }
+    }
+passport.use(new Localstrategy({usernameField:"email"},authenticateuser))
+passport.serializeUser((user,done)=>{done(null,user._id)})
+passport.deserializeUser((_id,done)=> { return done(null,getUserById(_id))})
+}
+module.exports = initilaize
+ 
+
+
+
+
