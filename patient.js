ce0a593b4842b57c10168a4816bcf00f476a386b
Galal <102048390+GalalAH@users.noreply.github.com>
2024-05-27 00:56:22 +0300

    Update patient.js

--- patient.js
+++ patient.js
@@ -86,7 +86,7 @@ async email => {
 
 router.post('/signup',async (req,res)=>{
   
-  let {password,email,name,phone,gender} = req.body 
+  let {password,email,name,phone,gender} = req.query
   try{
     if(!req.files){return res.json({message:"no image was uploaded",status:404})}
     const emailcheck =await patientUser.exists({Email:email})
