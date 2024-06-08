a87e17ca953e7120a9b97b2e755540cd7cf78077
Galal <102048390+GalalAH@users.noreply.github.com>
2024-06-09 01:07:07 +0300

    Update patient.js

--- patient.js
+++ patient.js
@@ -330,7 +330,9 @@ router.post('/emailverification',(req,res)=>{
         let {userId,month,Year,dayOfMonth} =req.body
         console.log(userId)
         Schedule.find({userId:userId,dayOfMonth,Year,month,"timeSlots": { $elemMatch: { available: true } }})
-        .then(result=>{if(result){res.json({message:"here the avalible appoinments ",result.timeSlots,status:200})}
+        .then(result=>{if(result)
+       const  slots=result.timeSlots
+        {res.json({message:"here the avalible appoinments ", slots,status:200})}
         else{res.json({message:"wrong id",status:404})}
         })
         .catch(err=>{console.log("err viewing apoinment : ",err)
